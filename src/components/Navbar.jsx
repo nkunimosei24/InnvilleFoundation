a5f5a8bfcc4827f81a6af55fef42b8cd36f19e53
nkunimosei24 <oseinkunim@gmail.com>
2026-07-15 09:30:02 +0000

    Nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -112,7 +112,7 @@ export default function Navbar() {
             state={{ reason: "Partnership / Sponsorship" }}
             className="mt-2 bg-red-500 text-white text-center px-4 py-3 rounded-md text-sm font-medium hover:bg-red-600 active:bg-red-700 transition-colors"
           >
-            Partner With Us
+            Job Vacancies
           </Link>
         </div>
       </div>
